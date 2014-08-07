19a979e627c92dd89c1154ed9b7ca5e9fd3c4ce3
ed wassermann <vis0vis@gmail.com>
2014-08-08 01:13:44 +0200

    Adjusts indentation in app.rb

--- app.rb
+++ app.rb
@@ -16,57 +16,57 @@ get('/javascripts/application.js'){ coffee :application }
 get('/application.css'){ sass :application }
 
 get '/' do
-	@title = ' :home'
-	@episodes = Episode.all
+  @title = ' :home'
+  @episodes = Episode.all
   slim :home, layout: :knowledge_bomb_layout
 end
 
 get '/episodes/new' do
-	@title = ' :new episode'
+  @title = ' :new episode'
   @episode = Episode.new
-	slim :new_episode 
+  slim :new_episode 
 end
 
 get '/episodes/:id' do
-	@title = ' :shownotes'
-	@episode = find_episode
-	slim :show_episode
+  @title = ' :shownotes'
+  @episode = find_episode
+  slim :show_episode
 end
 
 get '/episodes/:id/edit' do
-	@title = ' :edit'
-	@episode = find_episode
+  @title = ' :edit'
+  @episode = find_episode
   slim :edit_episode 
 end
 
 put '/episodes/:id' do
-	episode = find_episode
+  episode = find_episode
   episode.update(params[:episode])
-	redirect to("/episodes/#{ episode.id }")
+  redirect to("/episodes/#{ episode.id }")
 end
 
 delete '/episodes/:id' do
-	episode = find_episode
-	episode.destroy
-	redirect to('/')
+  episode = find_episode
+  episode.destroy
+  redirect to('/')
 end
 
 post '/episodes' do
   episode = Episode.create(params[:episode])
-	redirect to("/episodes/#{ episode.id }")
+  redirect to("/episodes/#{ episode.id }")
 end
 
 
 get '/about' do
-	@title = ' :about'
-	slim :about
+  @title = ' :about'
+  slim :about
 end
 
 get '/topics' do
-	@title = ' :topics'
-	slim :topics
+  @title = ' :topics'
+  slim :topics
 end
 
 def find_episode
-	Episode.get(params[:id])
+  Episode.get(params[:id])
 end
