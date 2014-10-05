51bcb8218a9f0e2168c0389b1c75d2372336fa58
ed wassermann <vis0vis@gmail.com>
2014-10-05 16:01:22 +0200

    Less items to paginate for testing

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ get('/application.css'){ sass :application }
 
 get '/' do
   @title = ': home'
-  @episodes = Episode.all(order: [:released_on.desc]).paginate(:page => params[:page], :per_page => 2)
+  @episodes = Episode.all(order: [:released_on.desc]).paginate(:page => params[:page], :per_page => 1)
   slim :home, layout: :knowledge_bomb_layout
 end
 
